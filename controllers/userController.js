7952d0e13637f2276d35a7f27700de0e18d5343c
Stalloyde <stalloyde93@gmail.com>
2024-01-07 18:29:22 +0800

    Rename module

--- controllers/userController.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-
-exports.user = (req, res, next) => {
-  res.json('User Route');
-};
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const indexController = require('../controllers/userController');
-
-const router = express.Router();
-
-/* GET users listing. */
-router.get('/', indexController.user);
-
-module.exports = router;
