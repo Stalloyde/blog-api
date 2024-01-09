c0765b3051eefa14f391692eb7b67fb439c9ee30
Stalloyde <stalloyde93@gmail.com>
2024-01-09 11:23:26 +0800

    Initialising passport

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('./config/passport');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
 
 const indexRouter = require('./routes/index');
 const adminRouter = require('./routes/admin');
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
