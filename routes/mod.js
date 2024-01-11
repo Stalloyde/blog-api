c1be5a8ef1406ba9ad017835c23e16eec9d87ff2
Stalloyde <stalloyde93@gmail.com>
2024-01-11 12:47:45 +0800

    Rename params

--- routes/mod.js
+++ routes/mod.js
@@ -8,11 +8,11 @@ const router = express.Router();
 /* GET admin listing. */
 router.get('/posts', passport.authenticate('jwt', { session: false }), isMod, modController.postGET);
 router.post('/posts', passport.authenticate('jwt', { session: false }), isMod, modController.postPOST);
-router.get('/posts/:postId', passport.authenticate('jwt', { session: false }), isMod, modController.postIdGET);
-router.put('/posts/:postId', passport.authenticate('jwt', { session: false }), isMod, modController.postIdPUT);
-router.delete('/posts/:postId', passport.authenticate('jwt', { session: false }), isMod, modController.postIdDEL);
-router.get('/posts/:postId/:commentId', passport.authenticate('jwt', { session: false }), isMod, modController.commentGET);
-router.put('/posts/:postId/:commentId', passport.authenticate('jwt', { session: false }), isMod, modController.commentPUT);
-router.delete('/posts/:postId/:commentId', passport.authenticate('jwt', { session: false }), isMod, modController.commentDEL);
+router.get('/posts/:id', passport.authenticate('jwt', { session: false }), isMod, modController.postIdGET);
+router.put('/posts/:id', passport.authenticate('jwt', { session: false }), isMod, modController.postIdPUT);
+router.delete('/posts/:id', passport.authenticate('jwt', { session: false }), isMod, modController.postIdDEL);
+router.get('/posts/:id/:commentId', passport.authenticate('jwt', { session: false }), isMod, modController.commentGET);
+router.put('/posts/:id/:commentId', passport.authenticate('jwt', { session: false }), isMod, modController.commentPUT);
+router.delete('/posts/:id/:commentId', passport.authenticate('jwt', { session: false }), isMod, modController.commentDEL);
 
 module.exports = router;
