35ed43d118116ee90ef797c366df9a47624a63d4
Stalloyde <stalloyde93@gmail.com>
2024-01-15 12:47:42 +0800

    Install cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const modRouter = require('./routes/mod');
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/mod', modRouter);
