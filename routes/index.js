cdb93368ab73f71aed58b45122421ba3a927bcfb
Stalloyde <stalloyde93@gmail.com>
2024-01-15 12:48:24 +0800

    Rename home path

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const indexController = require('../controllers/indexController');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', indexController.homeGET);
+router.get('/home', indexController.homeGET);
 router.get('/login', indexController.loginGET);
 router.post('/login', indexController.loginPOST);
 router.get('/signup', indexController.signupGET);
