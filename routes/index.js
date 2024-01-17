5f743d9f22fff5f5dab1f91762e253e33867afe3
Stalloyde <stalloyde93@gmail.com>
2024-01-17 16:15:18 +0800

    remove authentication to view all posts

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/login', indexController.loginGET);
 router.post('/login', indexController.loginPOST);
 router.get('/signup', indexController.signupGET);
 router.post('/signup', indexController.signupPOST);
-router.get('/posts', passport.authenticate('jwt', { session: false }), indexController.postGET);
+router.get('/posts', indexController.postGET);
 router.get('/posts/:id', passport.authenticate('jwt', { session: false }), indexController.postIdGET);
 router.post('/posts/:id', passport.authenticate('jwt', { session: false }), indexController.postPOSTComment);
 
