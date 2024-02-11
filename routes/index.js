314e2ea4f3b17700f30595cce9648c2d78e177da
Stalloyde <stalloyde93@gmail.com>
2024-02-11 14:52:30 +0800

    Add wildcard route

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,17 @@ const indexController = require('../controllers/indexController');
 const router = express.Router();
 
 /* GET home page. */
+router.get('*', indexController.postGET);
 router.get('/login', indexController.loginGET);
 router.post('/login', indexController.loginPOST);
 router.get('/signup', indexController.signupGET);
 router.post('/signup', indexController.signupPOST);
 router.get('/posts', indexController.postGET);
 router.get('/posts/:id', indexController.postIdGET);
-router.post('/posts/:id', passport.authenticate('jwt', { session: false }), indexController.postPOSTComment);
+router.post(
+  '/posts/:id',
+  passport.authenticate('jwt', { session: false }),
+  indexController.postPOSTComment,
+);
 
 module.exports = router;
