2880103875a1e2d1414f9e5c00e670b50b00fc23
Stalloyde <stalloyde93@gmail.com>
2024-02-12 14:42:08 +0800

    Undo - Try removing wildcard route but serve static files from index.html in app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public', 'index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(cors());
 
