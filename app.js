51eaaa77f87b871ccabef9b9c621418dc464a691
Stalloyde <stalloyde93@gmail.com>
2024-04-20 23:27:01 +0800

    Enable pre-flight cors

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(cors(corsOptions));
+app.options('*', cors());
 
 app.use('/', indexRouter);
 app.use('/mod', modRouter);
